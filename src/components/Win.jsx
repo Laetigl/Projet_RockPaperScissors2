4e4b97d83dc398918587d22839ff9caa4b1feac7
Laetitia Gallegos Lopez <laetitiagallegoslopez@MacBook-Air-de-Laetitia.local>
2024-02-16 11:54:45 +0100

    changes 2

--- src/components/Win.jsx
+++ src/components/Win.jsx
@@ -6,12 +6,14 @@ export default function Win(props) {
   let plus=()=>{
     props.setCounting(props.counting+1)
   }
+  
   return (
     <div className='flex flex-col justify-center items-center gap-[80px]'>
       <div className='flex gap-[80px] '>
         <p className='text-white text-[20px]'>YOU PICKED</p>
         <p className='text-white text-[20px]'>THE HOUSE PICKED</p>
       </div>
+      
       <div className='flex flex-col gap-[20px] justify-center items-center'>
         <h1 className='text-white text-[70px] font-bold'>YOU WIN</h1>
         <Restart setUserChoice={props.setUserChoice} setPickHouse ={props.setPickHouse} counter={plus}></Restart>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RockPaperScissors2/"
+  base: "/Projet_RockPaperScissors2/"
 })
